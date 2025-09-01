c088df2c6e17808005e6b515095edde1100594f9
Mendel Elmaleh <mendel@elmaleh.it>
2025-08-31 23:41:15 -0400

    Implement hebcal.HebrewDate

---
+++ hebcal/hebrewdate.go
@@ -0,0 +1,98 @@
+package hebcal
+
+import (
+	"time"
+
+	"github.com/mendelmaleh/conway/utils"
+)
+
+type HebrewMonth int
+
+const (
+	Nissan HebrewMonth = 1 + iota // TODO: +2?
+	Iyar
+	Sivan
+	Tamuz
+	Av
+	Elul
+	Tishrei
+	Cheshvan
+	Kislev
+	Tevet
+	Shevat
+	AdarI
+	AdarII
+)
+
+func (month HebrewMonth) String() string {
+	names := [...]string{
+		"Nissan",
+		"Iyar",
+		"Sivan",
+		"Tammuz",
+		"Av",
+		"Elul",
+		"Tishrei",
+		"Cheshvan",
+		"Kislev",
+		"Tevet",
+		"Shevat",
+		"Adar I",
+		"Adar II",
+	}
+
+	return names[int(month)-1]
+}
+
+func (month HebrewMonth) Partner() time.Month {
+	switch m := int(month); {
+	// nissan to elul is march to august
+	case month <= Elul:
+		return time.Month(m + 2)
+	// tishrei to adar ii is august to february
+	default:
+		return time.Month((m % 12) + 1)
+	}
+}
+
+type HebrewDate struct {
+	Year  int
+	Month HebrewMonth
+	Day   int
+}
+
+func (date *HebrewDate) Height() int {
+	romanyear := date.Year - 3760 // TODO: 3761?
+	he, _, _ := HeSheIt(romanyear - 1)
+	_, she, it := HeSheIt(romanyear)
+
+	switch date.Month {
+	case Tishrei, Cheshvan:
+		return date.Day + he
+	case Kislev:
+		return date.Day + max(he, she)
+	case Tevet, Shevat, AdarI, AdarII:
+		return date.Day + she
+	default:
+		return date.Day + it
+	}
+}
+
+func (date *HebrewDate) Roman() time.Time {
+	height := date.Height()
+	partner := date.Month.Partner()
+
+	value := int(partner)
+	// jan and feb are 13 and 14
+	if partner <= 2 {
+		value += 12
+	}
+
+	romanyear := date.Year - 3761
+	// source: github.com/bendory/conway-hebrew-calendar
+	if date.Month <= Elul || date.Month > Shevat || partner == time.January {
+		romanyear++
+	}
+
+	return utils.Date(romanyear, partner, height-value)
+}
---
+++ hebcal/hebrewdate_test.go
@@ -0,0 +1,86 @@
+package hebcal_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/mendelmaleh/conway/hebcal"
+	"github.com/mendelmaleh/conway/utils"
+)
+
+func TestHebrewMonth_Partner(t *testing.T) {
+	tests := []struct {
+		month hebcal.HebrewMonth
+		want  time.Month
+	}{
+		{hebcal.Tishrei, time.August},
+		{hebcal.Cheshvan, time.September},
+		{hebcal.Kislev, time.October},
+		{hebcal.Tevet, time.November},
+		{hebcal.Shevat, time.December},
+		{hebcal.AdarI, time.January},
+		{hebcal.AdarII, time.February},
+		{hebcal.Nissan, time.March},
+		{hebcal.Iyar, time.April},
+		{hebcal.Sivan, time.May},
+		{hebcal.Tamuz, time.June},
+		{hebcal.Av, time.July},
+		{hebcal.Elul, time.August},
+	}
+	for _, tt := range tests {
+		t.Run(tt.month.String(), func(t *testing.T) {
+			// TODO: construct the receiver type.
+			if got := tt.month.Partner(); got != tt.want {
+				t.Errorf("Partner() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestHebrewDate_Height(t *testing.T) {
+	tests := []struct {
+		date hebcal.HebrewDate
+		want int
+		name string // description of this test case
+	}{
+		{hebcal.HebrewDate{5760, hebcal.AdarII, 7}, 57, "moshe"},
+		{hebcal.HebrewDate{5779, hebcal.Iyar, 7}, 46, "paper example"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.date.Height(); got != tt.want {
+				t.Errorf("Height() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestHebrewDate_Roman(t *testing.T) {
+	tests := []struct {
+		date hebcal.HebrewDate
+		want time.Time
+		name string // description of this test case
+	}{
+		{
+			hebcal.HebrewDate{5760, hebcal.Tishrei, 1},
+			utils.Date(1999, time.September, 11), "rosh hashana",
+		},
+		{
+			hebcal.HebrewDate{5760, hebcal.AdarII, 7},
+			utils.Date(2000, time.March, 14),
+			"moshe",
+		},
+		{
+			hebcal.HebrewDate{5779, hebcal.Iyar, 7},
+			utils.Date(2019, time.May, 12),
+			"paper example",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.date.Roman(); got != tt.want {
+				t.Errorf("Roman() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
