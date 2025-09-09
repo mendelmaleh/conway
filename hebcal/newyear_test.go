7aaf7d90542ee616a918ffe3d75de7d7427281ab
Mendel Elmaleh <mendel@elmaleh.it>
2025-09-09 01:00:57 -0400

    Refactor hebcal/newyear_test.go

--- hebcal/newyear_test.go
+++ hebcal/newyear_test.go
@@ -1,6 +1,7 @@
 package hebcal_test
 
 import (
+	"strconv"
 	"testing"
 	"time"
 
@@ -8,26 +9,22 @@ import (
 	"github.com/mendelmaleh/conway/utils"
 )
 
+func fmtdate(t time.Time) string {
+	return t.Format(utils.ISO8601)
+}
+
 func TestNewYear(t *testing.T) {
-	tests := []struct {
-		year int
-		want time.Time
-		name string // description of this test case
-	}{
-		{1999, utils.Date(1999, time.September, 11), "1999"},
-		{2016, utils.Date(2016, time.October, 3), "2016"},
-		{2017, utils.Date(2017, time.September, 21), "2017"},
-		{2018, utils.Date(2018, time.September, 10), "2018"},
-		{2019, utils.Date(2019, time.September, 30), "paper example"},
+	tests := []time.Time{
+		utils.Date(1999, time.September, 11),
+		utils.Date(2016, time.October, 3),
+		utils.Date(2017, time.September, 21),
+		utils.Date(2018, time.September, 10),
+		utils.Date(2019, time.September, 30), // paper example
 	}
 	for _, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
-			if res := hebcal.NewYear(tc.year); res != tc.want {
-				t.Errorf(
-					"NewYear() = %v, want %v",
-					res.Format(utils.ISO8601),
-					tc.want.Format(utils.ISO8601),
-				)
+		t.Run(strconv.Itoa(tc.Year()), func(t *testing.T) {
+			if got := hebcal.NewYear(tc.Year()); got != tc {
+				t.Errorf("NewYear() = %v, want %v", fmtdate(got), fmtdate(tc))
 			}
 		})
 	}
