47ad1cf2c5a4f501082530a410fc9416564f0d96
Mendel Elmaleh <mendel@elmaleh.it>
2025-09-28 07:44:16 +0200

    Add utils.RomanNumeral()

--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,9 @@
 package utils
 
-import "time"
+import (
+	"strings"
+	"time"
+)
 
 const ISO8601 = "2006-01-02"
 
@@ -17,4 +20,19 @@ func Date(y int, m time.Month, d int) time.Time {
 // Leap checks if the year is leap
 func Leap(year int) bool {
 	return year%4 == 0 && (year%100 != 0 || year%400 == 0)
+}
+
+func RomanNumeral(n int) string {
+	val := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
+	sym := []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
+
+	var b strings.Builder
+	for i, v := range val {
+		for n >= v {
+			n -= v
+			b.WriteString(sym[i])
+		}
+	}
+
+	return b.String()
 }
