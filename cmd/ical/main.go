a59189f95f92ad23d0de5655f6a2330dc0d70fd5
Mendel Elmaleh <mendel@elmaleh.it>
2025-09-28 09:04:33 +0200

    Implement ical encoding

--- cmd/ical/main.go
+++ cmd/ical/main.go
@@ -2,23 +2,44 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"time"
 
+	"github.com/emersion/go-ical"
+	"github.com/google/uuid"
 	"github.com/mendelmaleh/conway/hebcal/holiday"
-	"github.com/mendelmaleh/conway/utils"
 )
 
 func main() {
-	year := 5789
+	year := 5786
 	diaspora := true
 
+	cal := ical.NewCalendar()
+	cal.Props.SetText(ical.PropProductID, "-//xyz Corp//NONSGML PDA Calendar Version 1.0//EN")
+	cal.Props.SetText(ical.PropVersion, "2.0")
+	cal.Props.SetText("X-WR-CALNAME", fmt.Sprintf("%d Hebrew calendar", year))
+	cal.Props.SetText("X-APPLE-CALENDAR-COLOR", "#FFCC00") // yellow
+
 	for _, v := range holiday.All {
-		for _, e := range v.Fill(year, diaspora) {
-			fmt.Println(
-				e.Date.Roman().Format(utils.ISO8601+" Mon:"),
-				e.Name,
-				e.Type.Is(holiday.Major),
-				e.Start,
-			)
+		for _, d := range v.Fill(year, diaspora) {
+			e := ical.NewEvent()
+			id, err := uuid.NewRandom()
+			if err != nil {
+				log.Fatal(err)
+			}
+			e.Props.SetText(ical.PropUID, id.String())
+			e.Props.SetDateTime(ical.PropDateTimeStamp, time.Now())
+
+			e.Props.SetDate(ical.PropDateTimeStart, d.Date.Roman())
+			e.Props.SetText(ical.PropSummary, d.Name)
+			e.Props.SetText(ical.PropDescription, fmt.Sprintf("%s, starts at %s", d.Type, d.Start))
+			cal.Children = append(cal.Children, e.Component)
 		}
+	}
+
+	enc := ical.NewEncoder(os.Stdout)
+	if err := enc.Encode(cal); err != nil {
+		log.Fatal(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/mendelmaleh/conway
 
 go 1.25.0
+
+require (
+	github.com/emersion/go-ical v0.0.0-20250609112844-439c63cef608
+	github.com/google/uuid v1.6.0
+)
+
+require github.com/teambition/rrule-go v1.8.2 // indirect
